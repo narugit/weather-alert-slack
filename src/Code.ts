f490fb1788c0f2835190f3d6beeb9e3f77ae5c07
narugit <narusens@gmail.com>
2019-03-24 06:24:04 +0900

    地点のjson化

--- src/Code.ts
+++ src/Code.ts
@@ -1,39 +1,39 @@
 // 型定義
 var webhookURL: string = "https://hooks.slack.com/services/xxxxxxxxxxxxxxxxxxxxxxxxxxx";
 var username: string = "weather-bot";  // 通知時に表示されるユーザー名
-var icon: string = ":hatching_chick:";  // 通知時に表示されるアイコン
-// var datas: object[] = [
-//     { 'place': '練馬', 'url': 'https://tenki.jp/amedas/3/16/44071.html' },
-//     { 'place': '東京', 'url': 'https://tenki.jp/amedas/3/16/44132.html' }
-// ];
+var icon: string = ":rain:";  // 通知時に表示されるアイコン
+var datas: any = [
+    { "place": "練馬", "url": "https://tenki.jp/amedas/3/16/44071.html" },
+    { "place": "東京", "url": "https://tenki.jp/amedas/3/16/44132.html" }
+];
 
 
 function alert(): void {
-    var message = scrape("練馬", "https://tenki.jp/amedas/3/16/44071.html");
+    Object.keys(datas).forEach(function (key) {
+        var message = scrape(datas[key].place, datas[key].url);
+        var jsonData: any =
+        {
+            "username": username,
+            "icon_emoji": icon,
+            "text": message
+        };
 
-    var jsonData: any =
-    {
-        "username": username,
-        "icon_emoji": icon,
-        "text": message
-    };
+        var payload: string = JSON.stringify(jsonData);
 
-    var payload: string = JSON.stringify(jsonData);
+        var options: any =
+        {
+            "method": "post",
+            "contentType": "application/json",
+            "payload": payload
+        };
 
-    var options: any =
-    {
-        "method": "post",
-        "contentType": "application/json",
-        "payload": payload
-    };
-
-    if (message !== 'fine') {
-        UrlFetchApp.fetch(webhookURL, options);
-    }
+        if (message !== 'fine') {
+            UrlFetchApp.fetch(webhookURL, options);
+        }
+    });
 }
 
 function scrape(place: string, url: string): string {
-    Logger.log(place);
     var resHTML = UrlFetchApp.fetch(url).getContentText();
 
     var regPrecipitation = /<li>.*10分値.*mm<\/li>/;
@@ -43,9 +43,7 @@ function scrape(place: string, url: string): string {
     var regDate = /<time.*現在<\/time>/;
     var dateTag = resHTML.match(regDate)!;
     var date = dateTag.toString().match(/\d*日\d*:\d*現在/)![0];
-
     Logger.log(precipitation);
-
     if (precipitation !== '0.0') {
         return (place + ': (' + date + ') 降水量 ' + precipitation + 'mm');
     }
