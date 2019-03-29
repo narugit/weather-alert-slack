3011bf10d5991be55347c80b72e83337f40eea2e
narugit <narusens@gmail.com>
2019-03-29 17:46:53 +0900

    error log 整形

--- src/Code.ts
+++ src/Code.ts
@@ -1,34 +1,38 @@
 // 型定義
 var webhookURL: string = "https://hooks.slack.com/services/xxxxxxxxxxxxxxxxxxxxxxxxxxx";
 var username: string = "weather-bot";  // 通知時に表示されるユーザー名
-var icon: string = ":rain:";  // 通知時に表示されるアイコン
+var icon: string = ":rain_cloud:";  // 通知時に表示されるアイコン
 var datas: any = [
     { "place": "練馬", "url": "https://tenki.jp/amedas/3/16/44071.html" },
     { "place": "東京", "url": "https://tenki.jp/amedas/3/16/44132.html" }
 ];
 
 
-function alert(): void {
+function alert(message: string): void {
+    var jsonData: any =
+    {
+        "username": username,
+        "icon_emoji": icon,
+        "text": message
+    };
+
+    var payload: string = JSON.stringify(jsonData);
+
+    var options: any =
+    {
+        "method": "post",
+        "contentType": "application/json",
+        "payload": payload
+    };
+
+    UrlFetchApp.fetch(webhookURL, options);
+}
+
+function CheckRain() {
     Object.keys(datas).forEach(function (key) {
-        var message = scrape(datas[key].place, datas[key].url);
-        var jsonData: any =
-        {
-            "username": username,
-            "icon_emoji": icon,
-            "text": message
-        };
-
-        var payload: string = JSON.stringify(jsonData);
-
-        var options: any =
-        {
-            "method": "post",
-            "contentType": "application/json",
-            "payload": payload
-        };
-
-        if (message !== 'fine') {
-            UrlFetchApp.fetch(webhookURL, options);
+        var message: string = scrape(datas[key].place, datas[key].url);
+        if (message !== "fine") {
+            alert(message);
         }
     });
 }
@@ -38,13 +42,14 @@ function NoticeError(err: string, logResHTML: string, logPrecipitationTag: any, 
     {
         "username": username,
         "icon_emoji": icon,
-        "text": "error message: " + err +
+        "text": "\n********************************\n" +
+            "error message: \n" + err +
             "\n********************************\n" +
-            "HTML: " + logResHTML +
+            "HTML: \n" + logResHTML +
             "\n********************************\n" +
-            "precipitationTag: " + logPrecipitationTag +
+            "precipitationTag: \n" + logPrecipitationTag +
             "\n********************************\n" +
-            "precipitation: " + logPrecipitation +
+            "precipitation: \n" + logPrecipitation +
             "\n********************************\n"
     };
 
@@ -64,10 +69,11 @@ function scrape(place: string, url: string): string {
     var resHTML = UrlFetchApp.fetch(url).getContentText();
 
     var regPrecipitation = /<li>.*10分値.*mm<\/li>/;
-    var precipitationTag = resHTML.match(regPrecipitation)!;
+    var precipitationTag;
     var precipitation = "";
 
     try {
+        precipitationTag = resHTML.match(regPrecipitation)!;
         precipitation = precipitationTag.toString().match(/\d*\.\d*/)![0];
     }
     catch (e) {
