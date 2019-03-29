be2e1a8c3826275af9d8764850cc0a455a8865b8
narugit <narusens@gmail.com>
2019-03-29 17:35:27 +0900

    error message出力

--- src/Code.ts
+++ src/Code.ts
@@ -33,26 +33,62 @@ function alert(): void {
     });
 }
 
+function NoticeError(err: string, logResHTML: string, logPrecipitationTag: any, logPrecipitation: any): void {
+    var jsonData: any =
+    {
+        "username": username,
+        "icon_emoji": icon,
+        "text": "error message: " + err +
+            "\n********************************\n" +
+            "HTML: " + logResHTML +
+            "\n********************************\n" +
+            "precipitationTag: " + logPrecipitationTag +
+            "\n********************************\n" +
+            "precipitation: " + logPrecipitation +
+            "\n********************************\n"
+    };
+
+    var payload: string = JSON.stringify(jsonData);
+
+    var options: any =
+    {
+        "method": "post",
+        "contentType": "application/json",
+        "payload": payload
+    };
+
+    UrlFetchApp.fetch(webhookURL, options);
+}
+
 function scrape(place: string, url: string): string {
     var resHTML = UrlFetchApp.fetch(url).getContentText();
 
     var regPrecipitation = /<li>.*10分値.*mm<\/li>/;
     var precipitationTag = resHTML.match(regPrecipitation)!;
-    var precipitation = precipitationTag.toString().match(/\d*\.\d*/)![0];
+    var precipitation = "";
+
+    try {
+        precipitation = precipitationTag.toString().match(/\d*\.\d*/)![0];
+    }
+    catch (e) {
+        NoticeError(e, resHTML, precipitationTag, precipitation);
+    }
 
     var regDate = /<time.*現在<\/time>/;
     var dateTag = resHTML.match(regDate)!;
     var date = dateTag.toString().match(/\d*日\d*:\d*現在/)![0];
+
     Logger.log(precipitation);
-    if (precipitation !== '0.0') {
-        return (place + ': (' + date + ') 降水量 ' + precipitation + 'mm');
+
+    if (precipitation !== "0.0") {
+        return (place + ": (" + date + ") 降水量 " + precipitation + "mm");
     }
 
-    else if (precipitation === '0.0') {
-        return 'fine';
+    else if (precipitation === "0.0") {
+        return "fine";
     }
 
     else {
-        return 'err';
+        return "err";
     }
 }
